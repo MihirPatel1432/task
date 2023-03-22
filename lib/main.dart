750097911953932dfa3a2b5e1a1cb249864f71a9
Mihir <mihirpatelmrp1432@gmail.com>
2023-03-23 00:06:58 +0530

    Project Setup Add Logic for color change

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyPage(),
+    );
+  }
+}
+
+
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  final Random _random = Random();
+
+  Color _color = const Color(0xFFFFFFFF);
+
+
+  ///[changeColor] function use for color change
+  void changeColor() {
+    setState(() {
+      _color = Color.fromARGB(
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InkWell(
+        onTap: changeColor,
+        child: Container(
+          color: _color,
+          alignment: Alignment.center,
+          child: const Text('Hey there'),
+        ),
+      ),
+    );
+  }
+}
+
